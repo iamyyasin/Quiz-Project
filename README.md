749ff9b3bd8d6b0bea452ebe5f72f8304e934ea7
iamyyasin <ozboga123@hotmail.com>
2023-09-03 00:25:24 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+<h1> Github Proje-2 </h1># proje-2
---
+++ main.js
@@ -0,0 +1,124 @@
+//! Quiz
+const quizInformation = [
+    {
+        //? First Quiz
+        questiontitle: 'Most active Twitter User?',
+        a: 'Fatih Kadir Akın',
+        b: 'İlker Kurtel',
+        c: 'Didem Küçükkaraaslan',
+        d: 'Azmi Mengü',
+        e: 'Onur Aslan',
+        correct: 'a' 
+    },
+    {
+        //? Second Quiz
+        questiontitle: 'Funniest Twitter User?',
+        a: 'Fatih Kadir Akın',
+        b: 'İlker Kurtel',
+        c: 'Didem Küçükkaraaslan',
+        d: 'Azmi Mengü',
+        e: 'Onur Aslan',
+        correct: 'b' 
+    },
+    {
+        //? Thirth Quiz
+        questiontitle: 'Top Twitter User?',
+        a: 'Fatih Kadir Akın',
+        b: 'İlker Kurtel',
+        c: 'Didem Küçükkaraaslan',
+        d: 'Azmi Mengü',
+        e: 'Onur Aslan',
+        correct: 'c' 
+    },
+    {
+        //? Fourth Quiz
+        questiontitle: 'Most Prolific Twitter User?',
+        a: 'Fatih Kadir Akın',
+        b: 'İlker Kurtel',
+        c: 'Didem Küçükkaraaslan',
+        d: 'Azmi Mengü',
+        e: 'Onur Aslan',
+        correct: 'd' 
+    },
+    {
+        //Todo: Finally Quiz
+        questiontitle: 'Co-founder zetupme?',
+        a: 'Fatih Kadir Akın',
+        b: 'İlker Kurtel',
+        c: 'Didem Küçükkaraaslan',
+        d: 'Azmi Mengü',
+        e: 'Onur Aslan',
+        correct: 'e'
+    }
+]
+
+//! Main Part
+const quiz = document.getElementById('questions');
+const titles = document.getElementById('questiontitles');
+const answers = document.querySelectorAll('.answer');
+const button = document.getElementById('continue');
+
+//TODO: Labels Part
+const a_text = document.getElementById('ans_a');
+const b_text = document.getElementById('ans_b');
+const c_text = document.getElementById('ans_c');
+const d_text = document.getElementById('ans_d');
+const e_text = document.getElementById('ans_e');
+
+let startQuiz = 0;
+let score = 0;
+
+//! Quiz Change
+insQuiz();
+
+function insQuiz() {
+    const activeQuiz = quizInformation[startQuiz];
+
+    titles.innerText = quizInformation[0].questiontitle;
+    a_text.innerText = quizInformation[0].a;
+    b_text.innerText = quizInformation[0].b;
+    c_text.innerText = quizInformation[0].c;
+    d_text.innerText = quizInformation[0].d;
+    e_text.innerText = quizInformation[0].e;
+}
+
+function disableAnswers() {
+    answers.forEach((answer) => (answer.checked = false));
+}
+
+function getSelected() {
+    let ans;
+
+    answers.forEach((answer) => {
+        if(answer.checked) {
+            ans = answer.id
+        }
+    })
+    return ans;
+}
+
+//? Click Button
+button.addEventListener('click', (event)=> {
+    event.preventDefault();
+    const ans = getSelected();
+
+    // console.log(ans);
+
+    if (ans) {
+        if (ans === quizInformation[startQuiz].correct) {
+            score++;
+        }
+        startQuiz++;
+
+        if (startQuiz < quizInformation.length) {
+            insQuiz();
+        }else {
+            quiz.style.height='180px';
+            quiz.style.width='100%';
+            quiz.innerHTML = `
+                <h2> The test is complete, ${score * 20} you have scored.🥳 </h2>
+                <button id="continue" onClick="location.reload()"> Try again 🏆</button>
+            `
+        }
+    }
+})
